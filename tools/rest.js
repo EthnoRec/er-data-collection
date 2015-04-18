4c755f336307924ab043e1e074f3e26cd1540432
Lee Archer <la12808@my.bristol.ac.uk>
2015-04-18 14:00:12 +0100

    Update profile.

--- tools/rest.js
+++ tools/rest.js
@@ -6,6 +6,8 @@ location.guadalajara = {lat: 20.659699, long: -103.349609};
 location.manchester = {lat: 53.480759, long: -2.242631};
 location.busan = {lat: 35.166667, long:129.066667};
 location.mumbai = {lat: 18.975, long: 72.825833};
+location.belohorizonte = {lat: -19.916667, long: -43.933333};
+location.monterrey = {lat: 25.666667, long:-100.3};
 
 var host = "http://localhost:3000";
 
@@ -119,11 +121,33 @@ if (command == "djob") {
             describe:"Set location - ["+Object.keys(location).join("/")+"]",
             alias: "location"
         })
+        .option("gender",{
+            type:"string",
+            describe:"Your gender [male|female]"
+        })
+        .option("gender-filter",{
+            type:"string",
+            describe:"Looking for [male|female]"
+        })
+        .option("distance-filter",{
+            describe:"Scan distance filter"
+        })
+        .option("age-filter-min",{
+            describe:"Minimum age"
+        })
+        .option("age-filter-max",{
+            describe:"Maximum age"
+        })
         .argv;
     var body = {};
-    if (ccargv.L) {
-        body.location = ccargv.L;
-    }
+    body.profile = {};
+    var int = parseInt;
+    if (ccargv.L) { body.location = ccargv.L; }
+    if (ccargv.gender) { body.profile.gender = int(ccargv.gender); }
+    if (ccargv["gender-filter"]) { body.profile.gender_filter = int(ccargv["gender-filter"]); }
+    if (ccargv["distance-filter"]) { body.profile.distance_filter = int(ccargv["distance-filter"]); }
+    if (ccargv["age-filter-min"]) { body.profile.age_filter_min = int(ccargv["age-filter-min"]); }
+    if (ccargv["age-filter-max"]) { body.profile.age_filter_max = int(ccargv["age-filter-max"]); }
     if (Object.keys(ccargv).length > 1) {
         request
             .put(host+"/tinder",{json:true,body:body},function(error,response,body){
